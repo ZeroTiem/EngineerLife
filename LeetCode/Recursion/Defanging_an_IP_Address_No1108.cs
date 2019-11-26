03f9bdb58e77c307771548c636426cec29aa0cd9
ZeroTiem <maxhank22@gmail.com>
2019-11-26 10:05:10 +0800

    Defanging_an_IP_Address_No1108

---
+++ LeetCode/Recursion/Defanging_an_IP_Address_No1108.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Recursion
+{
+    public class Defanging_an_IP_Address_No1108
+    {
+        public string DefangIPaddr(string address)
+        {
+            return address.Replace(".", "[.]");
+        }
+    }
+}
---
+++ LeetCodeTests/Recursion/Defanging_an_IP_Address_No1108Tests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Recursion;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Recursion.Tests
+{
+    [TestClass()]
+    public class Defanging_an_IP_Address_No1108Tests
+    {
+        [TestMethod()]
+        public void DefangIPaddrTest1()
+        {
+            //Arrange
+            var swapNodesinPairs = new Defanging_an_IP_Address_No1108();
+            var expected = "1[.]1[.]1[.]1";
+            //Act
+            var act = swapNodesinPairs.DefangIPaddr("1.1.1.1");
+            //Assert
+            Assert.AreEqual(expected, act);
+        }
+
+        [TestMethod()]
+        public void DefangIPaddrTest2()
+        {
+            //Arrange
+            var swapNodesinPairs = new Defanging_an_IP_Address_No1108();
+            var expected = "255[.]100[.]50[.]0";
+            //Act
+            var act = swapNodesinPairs.DefangIPaddr("255.100.50.0");
+            //Assert
+            Assert.AreEqual(expected, act);
+        }
+    }
+}
