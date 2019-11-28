c792415bb5764687b8e61278655df626cf8d6526
ZeroTiem <maxhank22@gmail.com>
2019-11-28 14:04:57 +0800

    Add No771_Jewels_and_Stones

---
+++ LeetCodeTests/Eazy/No771_Jewels_and_StonesTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Eazy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Eazy.Tests
+{
+    [TestClass()]
+    public class No771_Jewels_and_StonesTests
+    {
+        public No771_Jewels_and_Stones No771JewelsAndStones = new No771_Jewels_and_Stones();
+
+        [TestMethod()]
+        public void DefangIPaddrTest1()
+        {
+            //Arrange
+            var j = "aA";
+            var s = "aAAbbbb";
+            var expected = 3;
+            //Act
+            var act = No771JewelsAndStones.NumJewelsInStones(j,s);
+            //Assert
+            Assert.AreEqual(expected, act);
+        }
+
+        [TestMethod()]
+        public void DefangIPaddrTest2()
+        {
+            //Arrange
+            var j = "z";
+            var s = "ZZ";
+            var expected = 0;
+            //Act
+            var act = No771JewelsAndStones.NumJewelsInStones(j, s);
+            //Assert
+            Assert.AreEqual(expected, act);
+        }
+    }
+}
