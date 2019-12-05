3594546014392c97028db85fa2b28f5bc2f2ed26
ZeroTiem <maxhank22@gmail.com>
2019-12-05 13:28:59 +0800

    No1266_Minimum_Time_Visiting_All_Points

---
+++ LeetCode/Eazy/No1266_Minimum_Time_Visiting_All_Points.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace LeetCode.Eazy
+{
+    //Runtime: 92 ms, faster than 98.05% of C# online submissions for Minimum Time Visiting All Points.
+    //Memory Usage: 25.6 MB, less than 100.00% of C# online submissions for Minimum Time Visiting All Points.
+    public class No1266_Minimum_Time_Visiting_All_Points
+    {
+        public int MinTimeToVisitAllPoints(int[][] points)
+        {
+            var step = 0;
+            var row = points.Length;
+
+            for (int i = 0; i < row; i++)
+            {
+                var pointAx = points[i][0];
+                var pointAy = points[i][1];
+
+                if (i + 1 >= row)
+                    break;
+
+                var pointBx = points[i + 1][0];
+                var pointBy = points[i + 1][1];
+
+                var xStep = System.Math.Abs(pointAx - pointBx);
+                var yStep = System.Math.Abs(pointAy - pointBy);
+
+                if (xStep > yStep)
+                    step += xStep;
+                else
+                    step += yStep;
+            }
+
+            return step;
+        }
+    }
+}
---
+++ LeetCodeTests/Eazy/No1266_Minimum_Time_Visiting_All_PointsTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Eazy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Eazy.Tests
+{
+    [TestClass()]
+    public class No1266_Minimum_Time_Visiting_All_PointsTests
+    {
+        [TestMethod()]
+        public void BalancedStringSplitTest()
+        {
+            //Arrange
+            var no1221SplitAStringInBalancedStrings = new No1266_Minimum_Time_Visiting_All_Points();
+            var expected = 5;
+            //Act
+            var act = no1221SplitAStringInBalancedStrings.MinTimeToVisitAllPoints(new int[][]
+            {
+                new int[] { 3,2 },
+                new int[] { -2,2 },
+            });
+            //Assert
+            Assert.AreEqual(expected, act);
+        }
+
+        [TestMethod()]
+        public void BalancedStringSplitTest1()
+        {
+            //Arrange
+            var no1221SplitAStringInBalancedStrings = new No1266_Minimum_Time_Visiting_All_Points();
+            var expected = 7;
+            //Act
+            var act = no1221SplitAStringInBalancedStrings.MinTimeToVisitAllPoints(new int[][]
+            {
+                new int[] { 1,1 },
+                new int[] { 3,4 },
+                new int[] { -1,0 },
+            });
+            //Assert
+            Assert.AreEqual(expected, act);
+        }
+
+    }
+}
