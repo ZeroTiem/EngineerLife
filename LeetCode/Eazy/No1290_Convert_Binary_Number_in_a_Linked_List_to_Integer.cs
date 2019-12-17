03513900c42bef4587354f40fb9da8f3466d0bb3
ZeroTiem <maxhank22@gmail.com>
2019-12-17 14:15:11 +0800

    No1290_Convert_Binary_Number_in_a_Linked_List_to_Integer

---
+++ LeetCode/Eazy/No1290_Convert_Binary_Number_in_a_Linked_List_to_Integer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Eazy
+{
+    public class No1290_Convert_Binary_Number_in_a_Linked_List_to_Integer
+    {
+        private int Sum { get; set; }
+        private int Magnification { get; set; } = 0;
+
+        public int GetDecimalValue(ListNode head)
+        {
+            if (Magnification == 0)
+                Magnification = 1;
+            else
+                Magnification *= 2;
+
+            Sum += head.val == 1 ? Magnification : 0;
+
+            if (head.next != null)
+                GetDecimalValue(head.next);
+
+            return Sum;
+        }
+    }
+}
---
+++ LeetCodeTests/Eazy/No1290_Convert_Binary_Number_in_a_Linked_List_to_IntegerTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Eazy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Eazy.Tests
+{
+    [TestClass()]
+    public class No1290_Convert_Binary_Number_in_a_Linked_List_to_IntegerTests
+    {
+        [TestMethod()]
+        public void GetDecimalValueTest()
+        {
+            //Arrange
+            var no1290_Convert_Binary_Number_in_a_Linked_List_to_Integer = new No1290_Convert_Binary_Number_in_a_Linked_List_to_Integer();
+            var listNode1 = new ListNode(1);
+            var listNode2 = new ListNode(0);
+            var listNode3 = new ListNode(1);
+            listNode1.next = listNode2;
+            listNode2.next = listNode3;
+            var expected = 5;
+            //Act
+            var act = no1290_Convert_Binary_Number_in_a_Linked_List_to_Integer.GetDecimalValue(listNode1);
+            //Assert
+            Assert.AreEqual(expected, act);
+        }
+    }
+}
