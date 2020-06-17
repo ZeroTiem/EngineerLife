102354abdd93acf85d07da524e2c2402526b9ff4
ZeroTiem <maxhank22@gmail.com>
2020-06-17 13:30:26 +0800

    [LeetCode] 7. Reverse Integer

---
+++ LeetCode/Eazy/7. Reverse Integer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    /// <summary>
+    /// 7. Reverse Integer https://leetcode.com/problems/reverse-integer/
+    /// </summary>
+    public class ReverseInteger
+    {
+        public int Reverse(int x)
+        {
+            var index = 0 > x
+                ? 1
+                : 0;
+
+            if (Int32.TryParse(new string(x.ToString()
+                        .Substring(index)
+                        .Reverse()
+                        .ToArray()),
+                    out int resultValue) ==
+                false)
+            {
+                return 0;
+            }
+
+            //var resultValue = Convert.ToInt32();
+            return index == 0
+                ? resultValue
+                : resultValue * -1;
+        }
+    }
+}
---
+++ LeetCodeTests/Eazy/7. Reverse IntegerTests.cs
@@ -0,0 +1,65 @@
+﻿using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeTests
+{
+    [TestClass]
+    public class ReverseIntegerTests
+    {
+        [TestMethod]
+        public void Input123Oupt321()
+        {
+            //Arrange
+            var reverseInteger = new ReverseInteger();
+            var expected = 321;
+            var intput = 123;
+            //Act
+            var act = reverseInteger.Reverse(intput);
+            //Assert
+            Assert.AreEqual(expected, act);
+        }
+
+        [TestMethod]
+        public void InputNegative123OuptNegative321()
+        {
+            //Arrange
+            var reverseInteger = new ReverseInteger();
+            var expected = -321;
+            var intput = -123;
+            //Act
+            var act = reverseInteger.Reverse(intput);
+            //Assert
+            Assert.AreEqual(expected, act);
+        }
+
+        [TestMethod]
+        public void Input120Oupt21()
+        {
+            //Arrange
+            var reverseInteger = new ReverseInteger();
+            var expected = -321;
+            var intput = -123;
+            //Act
+            var act = reverseInteger.Reverse(intput);
+            //Assert
+            Assert.AreEqual(expected, act);
+        }
+
+        [TestMethod]
+        public void Input1534236469()
+        {
+            //Arrange
+            var reverseInteger = new ReverseInteger();
+            var expected = 0;
+            var intput = 1534236469;
+            //Act
+            var act = reverseInteger.Reverse(intput);
+            //Assert
+            Assert.AreEqual(expected, act);
+        }
+
+
+        
+    }
+
+}
