b0d681bcf5626a9cb450a88f56a450920e214313
ZeroTiem <maxhank22@gmail.com>
2020-12-14 20:16:27 +0800

    1.[Add] design pattern 2.[Add] FacadeSample

---
+++ DesignPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using FacadeSample1;
+
+namespace DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "123";
+            var output = MyTypeConverter.Toint("123");
+        }
+    }
+}
