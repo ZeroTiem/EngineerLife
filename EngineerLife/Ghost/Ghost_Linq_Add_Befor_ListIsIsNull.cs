f61d60d1dd5426fcbab1614e3b33ccb2d6b7df82
ZeroTiem <maxhank22@gmail.com>
2020-12-14 19:33:18 +0800

    Ghost_Linq_Add_Befor_ListIsIsNull

---
+++ EngineerLife/Ghost/Ghost_Linq_Add_Befor_ListIsIsNull.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EngineerLife.Ghost
+{
+    //https://stackoverflow.com/questions/48263237/ienumerable-and-lazy-loading
+    //As said above, deferred execution != lazy loading.
+
+    public class Ghost_Linq_Add_Befor_ListIsIsNull
+    {
+        public IEnumerable<(int key, int index)> AddList()
+        {
+            var reuslt = new List<int>();
+            var list = new List<(int key, int index)>
+            {
+                (key: 1, index: 1),
+                (key: 2, index: 1),
+                (key: 3, index: 1),
+                (key: 4, index: 1),
+                (key: 5, index: 1),
+                (key: 6, index: 1),
+                (key: 7, index: 1),
+                (key: 8, index: 1),
+                (key: 9, index: 1)
+            };
+            var selectList = list.Where(x => reuslt.Contains(x.key) == false);
+            reuslt.AddRange(selectList.Select(x => x.key));
+            return selectList;
+        }
+
+        public IEnumerable<int> AddListMode1()
+        {
+            var reuslt = new List<int>();
+            var list = new List<int>
+            {
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9
+            };
+            var selectList = list.Where(x => x < 10);
+            for (var i = 0; i < list.Count; i++)
+            {
+                list[i] = list[i] + 10;
+            }
+
+            list.ForEach(x => x = 10 + x);
+
+            foreach (int i in list)
+            {
+                //i = i + 10;
+            }
+
+            return selectList;
+        }
+
+        //public IEnumerable<(int key, int index)> AddListMode1()
+        //{
+        //    var reuslt = new List<int>();
+        //    var list = new List<(int key, int index)>
+        //    {
+        //        (key: 1, index: 1),
+        //        (key: 2, index: 1),
+        //        (key: 3, index: 1),
+        //        (key: 4, index: 1),
+        //        (key: 5, index: 1),
+        //        (key: 6, index: 1),
+        //        (key: 7, index: 1),
+        //        (key: 8, index: 1),
+        //        (key: 9, index: 1)
+        //    };
+        //    var selectList = list.Where(x => x.index == 1);
+        //    for (var i = 0; i < list.Count; i++)
+        //    {
+        //        //list[i].index = 10;
+        //        var item = list[i];
+        //        item.index++;
+        //        list[i] = item;
+        //    }
+        //    var print = selectList.ToList();
+        //    return selectList;
+        //}
+    }
+}
---
+++ EngineerLifeTests/Ghost/Ghost_Linq_Add_Befor_ListIsIsNullTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EngineerLife.Ghost;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EngineerLife.Ghost.Tests
+{
+    [TestClass()]
+    public class Ghost_Linq_Add_Befor_ListIsIsNullTests
+    {
+        [TestMethod()]
+        public void AddListTest()
+        {
+
+            //Arrange
+            var expected = new List<(int key, int index)>
+            {
+                (key: 1, index: 1),
+                (key: 2, index: 1),
+                (key: 3, index: 1),
+                (key: 4, index: 1),
+                (key: 5, index: 1),
+                (key: 6, index: 1),
+                (key: 7, index: 1),
+                (key: 8, index: 1),
+                (key: 9, index: 1)
+            };
+            var ghostLinqAddBeforListIsIsNull = new Ghost_Linq_Add_Befor_ListIsIsNull();
+            //Act
+            var act = ghostLinqAddBeforListIsIsNull.AddList();
+            //Assert
+            Assert.AreEqual(expected,act);
+        }
+
+        [TestMethod()]
+        public void AddListMode1Test()
+        {
+
+            //Arrange
+            var expected = new List<(int key, int index)>
+            {
+                (key: 1, index: 1),
+                (key: 2, index: 1),
+                (key: 3, index: 1),
+                (key: 4, index: 1),
+                (key: 5, index: 1),
+                (key: 6, index: 1),
+                (key: 7, index: 1),
+                (key: 8, index: 1),
+                (key: 9, index: 1)
+            };
+            var ghostLinqAddBeforListIsIsNull = new Ghost_Linq_Add_Befor_ListIsIsNull();
+            //Act
+            var act = ghostLinqAddBeforListIsIsNull.AddListMode1();
+            //Assert
+            Assert.AreEqual(expected, act);
+        }
+    }
+}
